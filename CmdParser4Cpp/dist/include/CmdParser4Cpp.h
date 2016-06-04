f9f1fe4a6aa7a5a8482214206eb96e6e0f43bcf5
Per Malmberg <per.malmberg@gmail.com>
2016-06-04 13:00:53 +0200

    Refactored to remove the need for separate members for each argument type, in turn reducing the need for separate access methods.

--- CmdParser4Cpp/dist/include/CmdParser4Cpp.h
+++ CmdParser4Cpp/dist/include/CmdParser4Cpp.h
@@ -14,6 +14,7 @@
 namespace cmdparser4cpp {
 
 class BoolType;
+
 class StringType;
 
 typedef std::vector<std::string> VectorOfString;
@@ -22,7 +23,7 @@ class CmdParser4Cpp
 {
 public:
 	explicit CmdParser4Cpp( IParseResult& parseResult );
-	
+
 	virtual ~CmdParser4Cpp();
 
 	// Defines an argument with the provided argument name.
@@ -42,37 +43,54 @@ public:
 	int GetAvailableBooleanParameterCount( const std::string& argumentName ) const;
 
 	template<typename ArgumentType, typename ValueType>
-	ValueType GetValue( const std::unordered_map<std::string, ArgumentType>& map, const std::string& argumentName, int index, ValueType defaultValue ) const;
+	ValueType GetValue(const std::string& argumentName, int index, ValueType defaultValue) const;
 
 	void GetUsage( IUsageFormatter& formatter ) const;
 
 private:
 	IParseResult& myParseResult;
 	std::unordered_map<std::string, std::shared_ptr<Argument>> myArguments;
-	std::unordered_map<std::string, const StringType*> myStringResults;
-	std::unordered_map<std::string, const BoolType*> myBoolResults;
+
+	// Create an envelope class that - by multiple inheritance - provides a common access interface to all
+	// the different argument types that we support. Using a struct instead of a class saves us from having to specify
+	// a public inheritance for each base class.
+	struct ResultEnvelope : std::unordered_map<std::string, const StringType*>,
+	                        std::unordered_map<std::string, const BoolType*>
+	{
+	};
+
+	ResultEnvelope myResults;
 
 	bool CheckArgumentTypes() const;
 	void RemoveEmptyArguments( std::vector<std::string>& arguments );
 	template<typename ArgumentType>
-	int GetAvailableParameterCount( const std::string& argumentName, std::unordered_map<std::string, const ArgumentType*> map ) const;
-	bool CheckConstraints( const VectorOfString& args );
+	int GetAvailableParameterCount(const std::string& argumentName) const;
+
+	bool CheckConstraints(const VectorOfString& args);
+
 	bool CheckMandatory() const;
+
 	bool CheckDependencies() const;
+
 	bool CheckMutualExclusion() const;
-	void GetIndexes( std::vector<std::pair<int, std::shared_ptr<Argument>>>& argumentIndexes, const std::vector<std::string>& arguments );
 
-	class IndexSorter {
+	void GetIndexes(std::vector<std::pair<int, std::shared_ptr<Argument>>>& argumentIndexes,
+	                const std::vector<std::string>& arguments);
+
+	class IndexSorter
+	{
 	public:
-		bool operator()(const std::pair<int, std::shared_ptr<Argument>>& lhs, const std::pair<int, std::shared_ptr<Argument>>& rhs) {
+		bool operator()(const std::pair<int, std::shared_ptr<Argument>>& lhs,
+		                const std::pair<int, std::shared_ptr<Argument>>& rhs)
+		{
 			return lhs.first < rhs.first;
-
 		}
 	};
 
 	// Prevent copying
-	CmdParser4Cpp( const CmdParser4Cpp& ) = delete;
-	CmdParser4Cpp& operator=( const CmdParser4Cpp& ) = delete;
+	CmdParser4Cpp(const CmdParser4Cpp&) = delete;
+
+	CmdParser4Cpp& operator=(const CmdParser4Cpp&) = delete;
 };
 
 } // END cmdparser4cpp
